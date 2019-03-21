4febc660958b1835244614fd3bb06247f912dd3c
ankokovin <lena13u@yandex.ru>
2019-03-21 16:34:11 +0500

    initial-commit

---
+++ TransformationComponent/TransformationComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TransformationComponent
+{
+    public class TransformationComponent
+    {
+        public string Transform(string text, string rules){
+            return text + rules;
+        }
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TransformationComponent;
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transf = new TransformationComponent.TransformationComponent();
+            Console.WriteLine(transf.Transform(args[0],args[1]));
+        }
+    }
+}
